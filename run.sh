e798d76a4ba8c21dd6c9e935997908505bcaa5d3
Quan Luong <lhongquan.1998@gmail.com>
2023-08-22 11:37:44 +0700

    init docker

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ git pull origin main
 docker build -t svelte-portfolio .
 docker stop svelte-portfolio
 docker rm svelte-portfolio
-docker run -p 9999:9999 --name svelte-portfolio -d --restart=always svelte-portfolio
+docker run -p 4444:4444 --name svelte-portfolio -d --restart=always svelte-portfolio
